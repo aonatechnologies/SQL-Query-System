7d8df4954bddcee1fa04f002d6f230049d9171e7
aonatechnologies <amit.baseball9@gmail.com>
2016-05-12 09:33:03 -0400

    Create Grader.java

---
+++ Grader.java
@@ -0,0 +1,9 @@
+public class Grader {
+	private String myTestLoc, myDirectory;
+	
+	public Grader(String testLoc, String directory){
+		this.myDirectory=directory;
+		this.myTestLoc=testLoc;
+	}
+	
+}
